05313e7c1891483de29311dbddc10298f71fdc9c
Astr0surf3r <astr0surf3r@gmail.com>
2018-09-26 22:49:41 -0700

    added capistrano environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :production
+server ENV['WEB_IP_ADDRESS'], user: ENV['WEB_USER'], roles: %w{app db web}
 
 # role-based syntax
 # ==================
