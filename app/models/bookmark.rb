953c64673e027997e78ef3ebd9baf967a0f2c171
kimi0806 <mhel_kimi@yahoo.com>
2019-07-13 23:13:49 +0200

    bookmark spec pass

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,9 +5,9 @@ class Bookmark < ActiveRecord::Base
 
   ###this is for rspec part
   attr_accessor :acts_as_taggable_on, :tags
-  def initialize (acts_as_taggable_on) 
+  def initialize (acts_as_taggable_on, belongs_to) 
     @acts_as_taggable_on = acts_as_taggable_on
-    
+    @belongs_to = belongs_to
   end
 
   ###
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe  'Bookmark' do
     it 'has to be taggable' do
         expect(@acts_as_taggable_on).to be_nil
     end
+    it 'should have user ' do
+        expect(@belongs_to).to be_nil
+    end
+    it 'should validate presence of url ' do
+        expect(:url).to be_truthy
+    end
+    it 'should validate presence of title ' do
+        expect(:title).to be_truthy
+    end
+    it 'should validate presence of tag ' do
+        expect(:tag_list).to be_truthy
+    end
+
 end
