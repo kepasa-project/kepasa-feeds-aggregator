7db2377b73ab0e9650de385bf376db98829a8262
kimi0806 <mhel_kimi@yahoo.com>
2019-08-29 10:45:48 +0200

    updated spec files and files

---
+++ spec/models/feed_entry_spec.rb
@@ -0,0 +1,14 @@
+require './spec/rails_helper.rb'
+require './spec/spec_helper.rb'
+require './app/models/feed_entry.rb'
+
+RSpec.describe Feed_Entry  do
+    #it 'has to mounted logo ' do
+    #    expect(@mount_uploader).to be_nil
+    #end
+
+    it 'has to have url feed' do
+        expect(self).to be_nil()
+    end
+end
+
