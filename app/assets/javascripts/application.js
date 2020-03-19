d895f987f1a025690d20eb42e52231fd12b87192
Astr0surf3r <astr0surf3r@gmail.com>
2020-03-18 23:53:28 -0700

    fixed toggle dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
 //= require jquery.infinitescroll
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
