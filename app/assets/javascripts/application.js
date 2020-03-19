d832deacdf048d6db08877d939abc96a7ae7d27d
Astr0surf3r <astr0surf3r@gmail.com>
2020-03-19 00:25:22 -0700

    added bootstrap line

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require jquery.infinitescroll
 //= require rails-ujs
 //= require activestorage
