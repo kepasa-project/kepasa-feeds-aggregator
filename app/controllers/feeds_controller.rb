c44e062e9cee1b2d1c6cf1e9f50215395db6e8b6
Astr0surf3r <astr0surf3r@gmail.com>
2020-04-24 20:19:20 -0700

    tmp

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -56,8 +56,82 @@ class FeedsController < ApplicationController
 
                     if @feed.save 
                     
-                      AddNewFeedWorker.perform_async(@feed.id)            
-                           
+                      #AddNewFeedWorker.perform_async(@feed.id)            
+#########inizio
+
+    @feed = Feed.find(feed_id)
+    @user = @feed.user
+
+    Feedjira::Feed.add_common_feed_entry_element("media:thumbnail", :value => :url, :as => :media_thumbnail_url)
+    Feedjira::Feed.add_common_feed_entry_element("enclosure", :value => :url, :as => :media_thumbnail_url)
+              
+    xml = HTTParty.get(@feed.rssurl).body
+
+    begin 
+      feed = Feedjira.parse(xml)  
+    rescue Exception => exc
+      logger.error("Message for the log file: #{exc.message} for the feed id: #{@feed.id}")
+      # added follow line for ASCCI-8BIT error 
+      xml.force_encoding("UTF-8")
+      feed = Feedjira.parse(xml)
+    end         
+    
+    #add last 10 news
+
+    feed.entries.each do |entry|  
+
+      entry.published.nil? ? @datafeedlist == Time.now() : @datafeedlist = entry.published
+
+      #unless Feedlist.where(:feed_id => @feed.id).exists? :guid => entry.id
+      unless Feedlist.where(:guid => entry.id).exists?
+
+        begin
+          @object = LinkThumbnailer.generate(entry.url)
+          @img_url = @object.images.last.to_s 
+        rescue Exception => exc
+          logger.error("Message for the log file: #{exc.message} for the feed id: #{@feed.id}")
+          @img_url = entry.image
+        end 
+
+        sleep 2
+        
+        @f = Feedlist.create!(
+               :rssurl       => @feed.rssurl,
+               :name         => entry.title,
+               :summary      => entry.summary,
+               :url          => entry.url,    
+               :published_at => @datafeedlist,
+               :guid         => entry.id,
+               :image        => entry.media_thumbnail_url,
+               :content      => entry.content,
+               :feed_id      => @feed.id,
+               :user_id      => @user.id
+             )
+        
+        begin
+          @f.remote_article_picture_url = @img_url
+          @f.save!
+        rescue Exception => exc
+          logger.error("Message for the log file: #{exc.message} for the feed id: #{@feed.id}")
+        end 
+
+        #store picture
+        unless @img_url.nil?
+          begin
+          a = Rails.root + "pictures/thumbnails"
+          path = "#{a}/feed-#{@feed.id}/#{@f.id}/"
+          system 'mkdir', '-p', path
+          #Dir.mkdir("#{path}") #unless File.exists?("#{path}")
+          download = open("#{@img_url}")
+          IO.copy_stream(download, "#{a}/feed-#{@feed.id}/#{@f.id}/#{download.base_uri.to_s.split('/')[-1]}")
+          rescue Exception => exc
+            logger.error("Message for the log file: #{exc.message} to create thumbnail directory: #{@f.id}")
+          end 
+        end
+
+      end
+    end
+####### fine
                       redirect_to root_path
 
                     else
