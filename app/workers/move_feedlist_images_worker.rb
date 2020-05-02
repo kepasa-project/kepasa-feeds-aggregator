7ad95c701f6ba2b56e5116fd2b10cd74a10b698c
Astr0surf3r <astr0surf3r@gmail.com>
2020-05-01 23:29:41 -0700

    fixing mv directory

--- app/workers/move_feedlist_images_worker.rb
+++ app/workers/move_feedlist_images_worker.rb
@@ -10,7 +10,7 @@ class MoveFeedlistImagesWorker
     a.pop
     #temporary email
     b = a.join("/") + "/20200327071103/public/uploads/feedlist/article_picture/@feedlist.id"
-    FileUtils.mv("#{b}", "home/kepasa/shared/public/uploads/feedlist/article_picture")
+    FileUtils.mv("#{b}", "/home/kepasa/shared/public/uploads/feedlist/article_picture")
   end
 
 end
