781389db25fed07a7bff45509e45df0e342f28c3
Astr0surf3r <astr0surf3r@gmail.com>
2020-05-10 23:56:24 -0700

    remove initializers for carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
 CarrierWave.configure do |config|
   # These permissions will make dir and files available only to the user running
   # the servers
-  config.permissions = 0600
-  config.directory_permissions = 0700
-  config.storage = :file
-  config.enable_processing = false
+  #config.permissions = 0600
+  #config.directory_permissions = 0700
+  #config.storage = :file
+  #config.enable_processing = false
+  
   # This avoids uploaded files from saving to public/ and so
   # they will not be available for public (non-authenticated) downloading
-  config.root = Rails.root
+  #config.root = Rails.root
 end
